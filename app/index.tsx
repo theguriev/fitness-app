9b3e9fddec3a3d23e980ef93c7bee85e5f746442
Eugen Guriev <gurievcreative@gmail.com>
2025-01-08 18:22:05 +0200

    feat: configure project with NativeWind and Tailwind CSS support

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
-import { Link } from 'expo-router';
-import { View, Text, StyleSheet } from 'react-native';
+import { Link } from "expo-router";
+import { View, Text, StyleSheet } from "react-native";
+
+import "../global.css";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text className="flex p-7 font-bold">Home</Text>
       <Link href="/login">View details</Link>
     </View>
   );
@@ -13,7 +15,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
