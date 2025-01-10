b2087e6fc840c869ce1df9fd07486a2e624061c6
Eugen Guriev <gurievcreative@gmail.com>
2025-01-10 20:08:34 +0200

    feat: update Tailwind config for component directory

--- components/Input.tsx
+++ components/Input.tsx
@@ -4,12 +4,14 @@ import { TextInput } from "react-native";
 
 const Input = ({
   error,
+  className,
   ...rest
 }: { error?: boolean } & ComponentProps<typeof TextInput>) => {
   return (
     <TextInput
       className={cn(
-        "w-full h-12 bg-white rounded-md px-4 mb-2 border border-gray-300",
+        className,
+        "w-full h-12 bg-white rounded-md px-4 mb-2 border border-[#000] text-bold",
         error && "border-red-500"
       )}
       {...rest}
