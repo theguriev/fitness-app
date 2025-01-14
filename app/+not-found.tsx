9731cbd5be1916377920e9fb901620ab86acc7ee
Eugen Guriev <gurievcreative@gmail.com>
2025-01-14 21:41:28 +0200

    feat: enhance NotFoundScreen with improved UI and navigation

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,29 +1,23 @@
-import { Link, Stack } from "expo-router";
-import { StyleSheet, View, Text } from "react-native";
+import { useNavigation } from "expo-router";
+import { View, Text } from "react-native";
+import Button from "@/components/Button";
 
 export default function NotFoundScreen() {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Stack.Screen options={{ title: "Oops!" }} />
-      <View style={styles.container}>
-        <Text>This screen doesn't exist.</Text>
-        <Link href="/" style={styles.link}>
-          <Text>Go to home screen!</Text>
-        </Link>
+    <View className="flex-1 items-center justify-center bg-gray-100">
+      <View className="p-8 ">
+        <Text className="text-6xl mb-4 text-center">😕</Text>
+        <Text className="text-2xl font-bold mb-2 text-center text-gray-800">
+          Oops! Page Not Found
+        </Text>
+        <Text className="text-center text-gray-600 mb-6">
+          The page you're looking for doesn't seem to exist.
+        </Text>
+        <Button onPress={() => navigation.navigate("index")}>
+          Go Back Home
+        </Button>
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
